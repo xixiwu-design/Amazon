3fa7710caa0753c88acae8c1d44ff7dc2ba8ee33
xixiwu-design <xixiwu@ceroca.cat>
2026-04-30 10:56:46 +0200

    crear paginas

---
+++ amazon/lib/components/barra.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Barra extends StatelessWidget {
+  const Barra({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
