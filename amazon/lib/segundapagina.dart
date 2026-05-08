1e42c0c8f3ce693bcd3871b7e7fc6ea6abbe8d3e
Juju-566 <judit_garrido@ceroca.cat>
2026-05-08 12:16:47 +0200

    texto1segundapajina

--- amazon/lib/segundapagina.dart
+++ amazon/lib/segundapagina.dart
@@ -7,14 +7,16 @@ class Segundapagina extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-   
-       body: SafeArea(
+      body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16),
           child: Column(
             children: [
               SizedBox(
-                child: SetFoto(url: "lib/images/anuncioPortatil.png",height: 100,),
+                child: SetFoto(
+                  url: "lib/images/anuncioPortatil.png",
+                  height: 100,
+                ),
               ),
 
               const SizedBox(height: 16),
@@ -22,7 +24,6 @@ class Segundapagina extends StatelessWidget {
               Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-
                   // Imagen izquierda
                   Container(
                     child: Stack(
@@ -36,35 +37,62 @@ class Segundapagina extends StatelessWidget {
                         ),
 
                         SizedBox(
-                          child: SetFoto(url: "lib/images/portatil.png",height:700,),
+                          child: SetFoto(
+                            url: "lib/images/portatil.png",
+                            height: 700,
+                          ),
                         ),
                       ],
                     ),
                   ),
 
-                  const SizedBox(width: 16),
+                  const SizedBox(width: 170),
 
-       // Texto 1
+                  // Texto 1
                   Expanded(
-                    
-                      child: const Text(
-                        '15,6 Pulgadas Ordenador Portátil Pantalla Táctil 2 en 1, Laptop 16GB RAM 512GB SSD, Win11 PC Portátil con Ryzen 3 3200U Procesador, Computadora Portátil 360° Pantalla Táctil 1080P FHD IPS Type-C RJ45\n\nbla bla bla',
-                        
-                      ),
-                      
-                    ),
-                  
+                    child: RichText(
+                      text: const TextSpan(
+                        style: TextStyle(color: Colors.black, fontSize: 14),
+                        children: [
+                          TextSpan(
+                            text: '15.6-Inch 2-in-1 Touchscreen Laptop, 16GB RAM 512GB SSD, Win11 Laptop PC with Ryzen 3 3200U Processor, 360° Convertible Touchscreen Laptop with 1080P FHD IPS Display Type-C RJ45',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text:
+                                '\n\n'
+                                'Brand:                  FFGUOK\n'
+                                'Model Name:       Laptop-50105-R3\n'
+                                'Color:                   Silver\n'
+                                'Hard Drive Size:  512 GB\n'
+                                'CPU Model:         Ryzen 3 3200U\n'
+                                'RAM Memory:     16 GB\n'
+                                'Installed OS:        Windows 11\n'
+                                'Special Features: HD Audio, Lightweight, Memory Card Slot,\n'
+                                '                              Numeric Keypad\n'
+                                'Graphics Card:    Integrated\n\n',
+                          ),
 
-                  const SizedBox(width: 16),
+                          TextSpan(
+                            text: 'About this item\n',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
 
-       // Texto 2
-                  Expanded(
-                    
-                      child: const Text(
-                        'Texto 2\n\nbla bla bla',
-                        
+                          TextSpan(
+                            text:
+                                '[Powerful Ryzen 3 Performance] Powered by the Ryzen 3 3200U processor with Radeon Vega 3 graphics, this 15.6-inch 2-in-1 laptop delivers smooth multitasking for work, study, and entertainment.\n\n'
+                                '[360° Touchscreen Design] The Full HD IPS touchscreen features vivid colors and a flexible 360° hinge, allowing use in laptop, tablet, tent, and stand modes.\n\n''[Fast Memory and Storage] Equipped with 16 GB DDR4 RAM and a 512 GB SSD for fast startup times, responsive performance, and efficient multitasking.\n'
+                                '[Modern Connectivity and Battery] Includes USB Type-C, USB 3.0, HDMI, RJ45, Wi-Fi 5, and Bluetooth 5.1. The 5000 mAh battery provides long-lasting daily use.\n\n''[Windows 11 and Multilingual Keyboard] Comes with Windows 11 preinstalled and interchangeable keyboard covers for multiple languages, backed by a 2-year warranty and lifetime support.\n',
+                          ),
+                        ],
                       ),
                     ),
+                  ),
+
+                  const SizedBox(width: 70),
+
+                  // Text 2
+                  Expanded(child: const Text('Text 2\n\nblah blah blah')),
                 ],
               ),
             ],
@@ -73,4 +101,4 @@ class Segundapagina extends StatelessWidget {
       ),
     );
   }
-  }
+}
